27af2adde8d26e05642d1231a82eb5d68d0e8878
Rafael Antunes <rafaelcantunes3@gmail.com>
2026-03-15 11:17:41 -0300

    adicionando log para debbug de conexao

--- internal/infra/repository/postgres.go
+++ internal/infra/repository/postgres.go
@@ -20,12 +20,11 @@ type PostgresConfig struct {
 func NewPostgresConnection(cfg PostgresConfig) *gorm.DB {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		cfg.Host, cfg.User, cfg.Password, cfg.DBName, cfg.Port, cfg.SSLMode)
-
+	log.Println("dsn montado para logar no RDS: ", dsn)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Falha ao conectar no banco de dados:", err)
 	}
-
 
 	fmt.Println(" Conexão com o PostgreSQL estabelecida com sucesso!")
 	return db
